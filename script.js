5a410af307e35a1d1f2cebf35f53a2c4da8beda5
willas <nascimentowillas5@gmail.com>
2024-10-16 16:50:52 -0300

    adcionei a imagem e a velocidade do player

--- script.js
+++ script.js
@@ -38,13 +38,19 @@ window.addEventListener('load', function(){
       this.height = 200;
       this.x = 0;
       this.y = this.gameHeight - this.height;
+      this.image = document.getElementById("playerImage");
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 1;
     }
     draw(context){
       context.fillStyle = 'white';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width,  this.frameY * this.height, 
+      this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(){
-      this.x++;
+      this.x += this.speed;
     }
 
   }
